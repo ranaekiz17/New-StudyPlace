e4784f02d2819d55f1810feed212304254652af2
ranaekiz17 <ekizgrana94@gmail.com>
2023-04-06 16:44:50 +0200

    ilk versiyon

---
+++ src/N_For_Loops/P01_ForLoop.java
@@ -0,0 +1,18 @@
+package N_For_Loops;
+
+public class P01_ForLoop {
+
+    public static void main(String[] args) {
+        /*
+        Kullanicidan baslangic ve bitis degeri olarak pozitif sayilar alin, sinirlar
+        dahil olarak aralarindaki tum sayilarin toplamini yazdirin.
+        Bitis degeri baslangic degerinden kucukse, uyari yazdirip islemi sonlandirin.
+        */
+
+        int bas = 20;
+        int bit = 40;
+
+        int sayiToplami=0;
+
+    }
+}
